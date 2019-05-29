978489b05ddc5a2624c625d1a58fcdc06283cb27
bykgh <bykxl@sina.com>
2019-05-29 11:31:59 +0800

    网关初始化完成

--- geteway/src/main/java/com/byk/geteway/GetewayApplication.java
+++ geteway/src/main/java/com/byk/geteway/GetewayApplication.java
@@ -2,8 +2,10 @@ package com.byk.geteway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GetewayApplication {
 
     public static void main(String[] args) {
