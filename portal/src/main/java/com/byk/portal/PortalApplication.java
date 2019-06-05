ad70a74e3d69b66e8ac805b4e430061cd5e875b3
bykgh <bykxl@sina.com>
2019-06-05 16:43:04 +0800

    调整

--- portal/src/main/java/com/byk/portal/PortalApplication.java
+++ portal/src/main/java/com/byk/portal/PortalApplication.java
@@ -6,11 +6,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringCloudApplication
 @EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PortalApplication {
+    //@EnableGlobalMethodSecurity 注解参考资料 https://www.jianshu.com/p/41b7c3fb00e0
 
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
