a76eb206c65900f401c9583ce84346800e2924c8
bykgh <bykxl@sina.com>
2019-06-11 15:57:58 +0800

    登录后首页初步建设

--- portal/src/main/java/com/byk/portal/controller/LoginController.java
+++ portal/src/main/java/com/byk/portal/controller/LoginController.java
@@ -1,18 +1,23 @@
 package com.byk.portal.controller;
 
+import com.byk.common.util.JsonHelper;
+import com.byk.common.util.StringUtil;
 import com.byk.portal.bean.Oauth2ToKenBean;
 import com.byk.portal.service.LoginService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.security.PermitAll;
+import java.io.IOException;
 
 @Controller
 public class LoginController {
@@ -42,17 +47,26 @@ public class LoginController {
     @RequestMapping(value="/loginSubmit")
     //@HystrixCommand(fallbackMethod = "loginError")
     @PermitAll
-    public String loginSubmit(String username,String password){
-        //Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        /*if (principal instanceof UserDetails) {
-            String username = ((UserDetails)principal).getUsername();
-        } else {
-            String username = principal.toString();
-        }*/
+    public String loginSubmit(String username, String password, Model model){
 
+        if (StringUtil.isNull(username) || StringUtil.isNull(password)){
+            return "login/loginError";
+        }
         Oauth2ToKenBean oauth2ToKenBean = loginService.signIn(username,password);
         if(oauth2ToKenBean != null){
-            logger.info("登录成功页面");
+            //token
+            model.addAttribute("access_token",oauth2ToKenBean.getAccess_token());
+            //用户信息
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+            if(principal != null){
+                try {
+                    logger.debug("登录成功页面 principal:{}", JsonHelper.toJson(principal));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
             return "login/loginSuccess";
         }
         return "login/loginError";
