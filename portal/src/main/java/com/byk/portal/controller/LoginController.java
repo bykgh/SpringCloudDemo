1016330fa8daacda1e74726c6f8c99e53b7327af
bykgh <bykxl@sina.com>
2019-06-29 21:57:33 +0800

    验证码登录

--- portal/src/main/java/com/byk/portal/controller/LoginController.java
+++ portal/src/main/java/com/byk/portal/controller/LoginController.java
@@ -1,15 +1,19 @@
 package com.byk.portal.controller;
 
+import com.byk.common.common.RedisCommon;
 import com.byk.common.util.StringUtil;
 import com.byk.portal.bean.Oauth2ToKenBean;
 import com.byk.portal.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.annotation.security.PermitAll;
 
 /**
@@ -23,6 +27,9 @@ public class LoginController {
 
     @Autowired
     private LoginService loginService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 跳转登录页面
@@ -39,18 +46,34 @@ public class LoginController {
      * 登录
      * @param username
      * @param password
+     * @param kaptchaVerCode 图形验证码
      * @return
      */
     @RequestMapping(value="/loginSubmit")
     @PermitAll
-    public String loginSubmit(String username, String password, Model model){
+    public String loginSubmit(String username, String password,String kaptchaVerCode, Model model){
 
         if (StringUtil.isNull(username) || StringUtil.isNull(password)){
+            model.addAttribute("stauts","FAILED");
+            model.addAttribute("msg","用户名密码不能为空");
             return "login/loginError";
         }
+
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        String verCode = ops.get(RedisCommon.KAPTCHA_VER_CODE + username);
+        if(verCode == null || !verCode.equalsIgnoreCase(kaptchaVerCode)){
+            model.addAttribute("stauts","FAILED");
+            model.addAttribute("msg","验证码错误");
+            return "login/loginError";
+        }else{
+            stringRedisTemplate.delete(RedisCommon.KAPTCHA_VER_CODE + username);
+        }
+
+
         Oauth2ToKenBean oauth2ToKenBean = loginService.signIn(username,password);
         if(oauth2ToKenBean != null){
             //token
+            model.addAttribute("msg","登录成功");
             model.addAttribute("stauts","SUCCESS");
             model.addAttribute("token_type",oauth2ToKenBean.getToken_type());
             model.addAttribute("access_token", oauth2ToKenBean.getAccess_token());
