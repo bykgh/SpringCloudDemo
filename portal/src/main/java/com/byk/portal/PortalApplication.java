0f4032f2c9cab302db5d9d18853bb5e29fcc3efc
bykgh <bykxl@sina.com>
2019-07-04 14:41:13 +0800

    配置请求头转发及session共享

--- portal/src/main/java/com/byk/portal/PortalApplication.java
+++ portal/src/main/java/com/byk/portal/PortalApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 启动类
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableFeignClients
 //@EnableGlobalMethodSecurity 注解参考资料 https://www.jianshu.com/p/41b7c3fb00e0
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableRedisHttpSession
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
