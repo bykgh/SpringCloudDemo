5403cc1e3df68b4b4ba5b7f97ede908be5e08e39
bykgh <bykxl@sina.com>
2019-07-04 09:59:31 +0800

    常量

---
+++ common/src/main/java/com/byk/common/common/PortalCommon.java
@@ -0,0 +1,12 @@
+package com.byk.common.common;
+
+/**
+ * @Author: love
+ * @Description: portal常量
+ * @Date: 2019-07-04 09:30
+ * @Version: 1.0
+ */
+public class PortalCommon {
+
+    public static final String VER_CODE_SESSION_KEY = "VER_CODE_SESSION_KEY";
+}
