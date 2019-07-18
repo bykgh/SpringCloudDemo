c196963479b5c821ddcf2fffcbbe285a1842caf3
bykgh <bykxl@sina.com>
2019-07-18 17:43:27 +0800

    监控中心初始化

--- admin/src/main/java/com/byk/admin/AdminApplication.java
+++ admin/src/main/java/com/byk/admin/AdminApplication.java
@@ -2,7 +2,6 @@ package com.byk.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
