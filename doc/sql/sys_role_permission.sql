b2f1c7dab1d3d296e34cb18ce84379b6efce6de1
bykgh <bykxl@sina.com>
2019-07-21 21:46:55 +0800

    sql文件更新

--- doc/sql/sys_role_permission.sql
+++ doc/sql/sys_role_permission.sql
@@ -1,31 +1,5 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50717
- Source Host           : localhost:3306
- Source Schema         : oms
-
- Target Server Type    : MySQL
- Target Server Version : 50717
- File Encoding         : 65001
-
- Date: 05/06/2019 15:17:41
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for sys_role_permission
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role_permission`;
-CREATE TABLE `sys_role_permission` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `sys_role_id` int(11) NOT NULL COMMENT '角色id',
-  `sys_permission_id` int(11) NOT NULL COMMENT '权限id',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='角色权限表(对角色和权限表的一个包装)';
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `sys_role_permission` VALUES (1, 1, 1);
+INSERT INTO `sys_role_permission` VALUES (2, 2, 1);
+INSERT INTO `sys_role_permission` VALUES (3, 1, 2);
+INSERT INTO `sys_role_permission` VALUES (4, 2, 2);
+INSERT INTO `sys_role_permission` VALUES (5, 1, 3);
