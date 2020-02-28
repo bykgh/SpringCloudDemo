10c31484a5c0c4e9fd36bcef0bedc7c116138a32
yikai.bi <yikai.bi@gmail.com>
2020-02-28 16:13:22 +0800

    zull网关配置完善

--- gateway/src/main/java/com/byk/gateway/config/WebSecurityConfig.java
+++ gateway/src/main/java/com/byk/gateway/config/WebSecurityConfig.java
@@ -1,37 +1,24 @@
 package com.byk.gateway.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+/**
+ * @author yikai.bi
+ */
 @EnableWebSecurity
-@Order(99)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
-     *HTTP请求安全处理
-     * @param
+     * 当前将所有请求放行,交给资源配置类进行资源权限判断
+     * 因为默认情况下会拦截所有请求
+     * @param http
      * @throws Exception
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
-
-    /**
-     * web 安全
-     * @param web
-     * @throws Exception
-     */
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers
-                ("/portal/templates/**", "/portal/static/**",
-                        "/account-server/assets/**","/**/favicon.ico");
+        http.authorizeRequests().anyRequest().permitAll();
     }
 
 }
